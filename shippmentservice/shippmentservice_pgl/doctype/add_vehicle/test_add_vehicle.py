7ba02aedd73d78c5122f916a62af7d517828e0b6
arahimfahim40 <57112494+arahimfahim40@users.noreply.github.com>
2021-05-25 14:22:07 -0500

    Add files via upload

---
+++ shippmentservice/shippmentservice_pgl/doctype/add_vehicle/test_add_vehicle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, me and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestAddVehicle(unittest.TestCase):
+	pass
