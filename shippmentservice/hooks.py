f5588aaf8db6c0a73cf795b5b3ed5c453467ded2
arahimfahim40 <57112494+arahimfahim40@users.noreply.github.com>
2021-05-25 14:17:40 -0500

    Add files via upload

---
+++ shippmentservice/hooks.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from . import __version__ as app_version
+
+app_name = "shippmentservice"
+app_title = "ShippmentService-PGL"
+app_publisher = "me"
+app_description = "Use for PGL Vehicle Shippment Service"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "marianoor09@outlook.com"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/shippmentservice/css/shippmentservice.css"
+# app_include_js = "/assets/shippmentservice/js/shippmentservice.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/shippmentservice/css/shippmentservice.css"
+# web_include_js = "/assets/shippmentservice/js/shippmentservice.js"
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "shippmentservice.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "shippmentservice.install.before_install"
+# after_install = "shippmentservice.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "shippmentservice.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"shippmentservice.tasks.all"
+# 	],
+# 	"daily": [
+# 		"shippmentservice.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"shippmentservice.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"shippmentservice.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"shippmentservice.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "shippmentservice.install.before_tests"
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "shippmentservice.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "shippmentservice.task.get_dashboard_data"
+# }
+
